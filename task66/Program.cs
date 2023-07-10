a83801927603e3f4b0c38e4e34e1fa23f501f63b
Valeria <133519509+Valeria0308@users.noreply.github.com>
2023-07-10 23:27:35 +0300

    added task66

---
+++ task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void Main()
+{
+    int M = ReadInt("Введите значение M");
+    int N = ReadInt("Введите значение N");
+    System.Console.WriteLine($"Сумма натуральных элементов от M до N равна: {SumNumbers(M, N)}");
+}
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int N, int M)
+{
+    if (N == M) return N;
+    return SumNumbers(N + 1, M) + N;
+}
+
+Main();
